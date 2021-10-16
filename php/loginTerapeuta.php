359278405d2e527dd76693f7cb55cccd3150af88
andrespolop <andrespolop@gmail.com>
2021-10-15 22:25:18 -0500

    loginysignup

---
+++ php/loginTerapeuta.php
@@ -0,0 +1,39 @@
+ <?php 
+
+$conectar = mysqli_connect("localhost", "root", "","psitas");
+
+if(mysqli_connect_errno()){
+    echo "Fallo al tratar de conectar con base de datos Registro";
+    exit();
+}
+
+//Recuperar variables del formulario
+
+
+$email = $_POST['email'];
+$contrasena = $_POST['contrasena'];
+
+// Hacemos la sentencia SQL
+
+$sql = "SELECT * FROM registro WHERE email = '$email' and contrasena = '$contrasena'";
+
+//EJECUTAMOS LA SENTENCIA CON mysql_query
+
+$ejecutar = mysqli_query($conectar,$sql);
+
+//verificamos la ejecución
+
+$fila = mysqli_num_rows($ejecutar);
+
+if($fila){
+    header("location:../terapeuta-citas.html");
+}else{
+    echo"<script> alert('Datos ingresados erróneos');
+    window.location = '/login.html'
+    </script>";
+}
+
+mysqli_free_result($ejecutar);
+mysqli_close($conectar);
+
+?>
---
+++ php/registrarTerapeuta.php
@@ -0,0 +1,35 @@
+<?php
+
+// Conexión base de datos
+
+$conectar = mysqli_connect("localhost","root","","psitas");
+if(mysqli_connect_errno()){
+    echo "Fallo al conectar con Base de Datos";
+    exit();
+}
+
+// Recuperar variables del formulario
+
+$nombre = $_POST['nombre'];
+$apellido = $_POST['apellido'];
+$email = $_POST['email'];
+$contrasena = $_POST['contrasena'];
+$telefono = $_POST['telefono'];
+
+// Creamos la sentencia SQL
+
+$sql = "INSERT INTO registro (nombre, apellido, email, contrasena, telefono) values('$nombre','$apellido','$email','$contrasena','$telefono')";
+
+//Ejecutar la sentencia SQL
+$ejecutar = mysqli_query($conectar,$sql);
+
+//Verificamos ejecución
+
+if(!$ejecutar){
+    echo "No se ejecutó";
+}else{
+    echo "<script>alert('Registro exitoso');
+    window.location = '../index.html';
+    </script>";
+}
+?>
