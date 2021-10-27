d69e5a6a6c0538cfd8e421180b6e1be60673b89e
andrespolop <andrespolop@gmail.com>
2021-10-27 17:27:11 -0500

    commentario

--- src/terapeuta-horarios.js
+++ src/terapeuta-horarios.js
@@ -11,14 +11,14 @@ xhr.onload = () => {
     horarios_disponibles = JSON.parse(xhr.responseText);
 
     var horas = [
-      "08:00",
-      "09:00",
-      "10:00",
-      "11:00",
-      "12:00",
-      "13:00",
-      "14:00",
-      "15:00",
+      "08",
+      "09",
+      "10",
+      "11",
+      "12",
+      "13",
+      "14",
+      "15",
     ];
 
     function diaCorrecto(x) {
@@ -198,11 +198,11 @@ xhr_dispo.onload = function () {
     var hora_new;
     var min_new;
     for (let i = 0; i < horarios_dispo_des.length; i++) {
-        
+        //Separador de fechas - horas
       if (horarios_dispo_des[i]["id_terapeuta"] == document.getElementById("id_terapeuta").value) {
         let horayminuto = horarios_dispo_des[i]["hora"].split(':');
-         hora_se = parseInt(horayminuto[0]);
-         min_se= parseInt(horayminuto[1]);
+        hora_se = parseInt(horayminuto[0]);
+        min_se= parseInt(horayminuto[1]);
         
         
         if (hora_se<10) {
