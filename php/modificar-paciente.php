8a94b0ffd4c8abdc1c5d4c86fd7237c4e16bfabf
Fernando Padilla <effjay.dev@gmail.com>
2021-10-29 15:24:10 -0500

    nuevos archivos citas-agendadas

---
+++ php/modificar-paciente.php
@@ -0,0 +1,27 @@
+<?php
+
+include("conexion.php");
+
+// Obtención de variables
+
+$id = $_POST['id'];
+$correo = $_POST['correo'];
+$telefono = $_POST['telefono'];
+$direccion = $_POST['direccion'];
+
+// Creamos el SQL
+
+
+
+$sql = "UPDATE usuarios SET correo = '$correo', telefono = '$telefono', direccion = '$direccion' WHERE id = '$id'";
+
+$resultado = mysqli_query($conexion, $sql);
+
+
+if ($resultado) {
+    echo "<script> alert('Se han actualizado los cambios correctamente.');
+    window.location = '../paciente-perfil.php'; </script>";
+} else {
+    echo "<script>alert('No se pudo actualizar los datos');
+    window.history.go(-1); </script>";
+}
