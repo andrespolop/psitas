826186007189a586abf13f14d573b4c9ef037aef
DACR-0 <dacr2312@gmail.com>
2021-10-29 14:39:55 -0500

    act index 0d

--- index.php
+++ index.php
@@ -6,10 +6,19 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
-    <link rel="stylesheet" href="estilo2.css">
+    <link rel="stylesheet" href="estilos/estilo2.css">
 </head>
 
 <body>
+<header>
+        <a href="index.php">
+            <h1 class="titulo_psitas">PSI-TAS</h1>
+        </a>
+        <a href="index.php">
+            <div class="imagen_icono"><img src="iconos/psitas_icon_NoBackground.jpg" alt="imagen_empresa"></div>
+        </a>
+    </header>
+
 <div class="caja">
 
 <center> <marquee style='width: 100%; color: blue;'><b>BIENVENIDOS A PSI-TAS</b></marquee> </center>
